827273713c6703e5cf73c9afdaa154b6153f0212
Arinadi Nur Rohmad <arinadi.nur@gmail.com>
2024-03-03 04:19:57 +0700

    update contact

--- assets/js/templatemo-custom.js
+++ assets/js/templatemo-custom.js
@@ -1,9 +1,9 @@
 (function ($) {
-	
-	"use strict";
 
-	// Header Type = Fixed
-  $(window).scroll(function() {
+  "use strict";
+
+  // Header Type = Fixed
+  $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     var box = $('.header-text').height();
     var header = $('header').height();
@@ -16,33 +16,33 @@
   });
 
 
-	$('.owl-our-team').owlCarousel({
-		items:3,
-		loop:true,
-		dots: true,
-		nav: false,
-		autoplay: true,
-		margin:0,
-		  responsive:{
-			  0:{
-				  items:1
-			  },
-			  600:{
-				  items:2
-			  },
-			  1000:{
-				  items:3
-			  },
-			  1600:{
-				  items:3
-			  }
-		  }
-	})
-	
+  $('.owl-our-team').owlCarousel({
+    items: 3,
+    loop: true,
+    dots: true,
+    nav: false,
+    autoplay: true,
+    margin: 0,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      },
+      1600: {
+        items: 3
+      }
+    }
+  })
 
-	// Menu Dropdown Toggle
-  if($('.menu-trigger').length){
-    $(".menu-trigger").on('click', function() { 
+
+  // Menu Dropdown Toggle
+  if ($('.menu-trigger').length) {
+    $(".menu-trigger").on('click', function () {
       $(this).toggleClass('active');
       $('.header-area .nav').slideToggle(200);
     });
@@ -50,16 +50,16 @@
 
 
   // Menu elevator animation
-  $('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  $('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         var width = $(window).width();
-        if(width < 991) {
+        if (width < 991) {
           $('.menu-trigger').removeClass('active');
-          $('.header-area .nav').slideUp(200);  
-        }       
+          $('.header-area .nav').slideUp(200);
+        }
         $('html,body').animate({
           scrollTop: (target.offset().top) + 1
         }, 700);
@@ -69,61 +69,61 @@
   });
 
   $(document).ready(function () {
-      $(document).on("scroll", onScroll);
-      
-      //smoothscroll
-      $('.scroll-to-section a[href^="#"]').on('click', function (e) {
-          e.preventDefault();
-          $(document).off("scroll");
-          
-          $('.scroll-to-section a').each(function () {
-              $(this).removeClass('active');
-          })
-          $(this).addClass('active');
-        
-          var target = this.hash,
-          menu = target;
-          var target = $(this.hash);
-          $('html, body').stop().animate({
-              scrollTop: (target.offset().top) + 1
-          }, 500, 'swing', function () {
-              window.location.hash = target;
-              $(document).on("scroll", onScroll);
-          });
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+
+      $('.scroll-to-section a').each(function () {
+        $(this).removeClass('active');
+      })
+      $(this).addClass('active');
+
+      var target = this.hash,
+        menu = target;
+      var target = $(this.hash);
+      $('html, body').stop().animate({
+        scrollTop: (target.offset().top) + 1
+      }, 500, 'swing', function () {
+        window.location.hash = target;
+        $(document).on("scroll", onScroll);
       });
+    });
   });
 
-  function onScroll(event){
-      var scrollPos = $(document).scrollTop();
-      $('.nav a').each(function () {
-          var currLink = $(this);
-          var refElement = $(currLink.attr("href"));
-          if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-              $('.nav ul li a').removeClass("active");
-              currLink.addClass("active");
-          }
-          else{
-              currLink.removeClass("active");
-          }
-      });
+  function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('.nav a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        $('.nav ul li a').removeClass("active");
+        currLink.addClass("active");
+      }
+      else {
+        currLink.removeClass("active");
+      }
+    });
   }
 
 
 
-	// Page loading animation
-	 $(window).on('load', function() {
+  // Page loading animation
+  $(window).on('load', function () {
 
-        $('#js-preloader').addClass('loaded');
+    $('#js-preloader').addClass('loaded');
 
-    });
+  });
 
-	
 
-	// Window Resize Mobile Menu Fix
+
+  // Window Resize Mobile Menu Fix
   function mobileNav() {
     var width = $(window).width();
-    $('.submenu').on('click', function() {
-      if(width < 767) {
+    $('.submenu').on('click', function () {
+      if (width < 767) {
         $('.submenu ul').removeClass('active');
         $(this).find('ul').toggleClass('active');
       }
@@ -155,3 +155,38 @@ document.getElementById('search').addEventListener('submit', function (event) {
   // Call the reusable function to send WhatsApp message
   sendWhatsAppMessage(message);
 });
+
+document.getElementById('contact').addEventListener('submit', function () {
+  // Get form data
+  var name = document.getElementById('name').value;
+  var surname = document.getElementById('surname').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+
+  // Construct the WhatsApp message
+  var whatsappMessage = encodeURIComponent(
+    "Name: " + name + "\n" +
+    "Surname: " + surname + "\n" +
+    "Email: " + email + "\n" +
+    "Message: " + message
+  );
+
+  // Call the sendWhatsAppMessage function with the constructed message
+  sendWhatsAppMessage(whatsappMessage);
+});
+
+
+var textIndex = 0;
+var texts = ["Code", "Build", "Deploy"];
+var flipText = document.getElementById("flipText");
+
+function flip() {
+    flipText.classList.add("flipOut");
+    setTimeout(function() {
+        flipText.innerText = texts[textIndex];
+        flipText.classList.remove("flipOut");
+        textIndex = (textIndex + 1) % texts.length;
+    }, 500); // Adjust timing accordingly
+}
+
+setInterval(flip, 2000); // Adjust interval accordingly
