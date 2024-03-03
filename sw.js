e73e405732e7c527478217532da47535d52e3ce5
Arinadi Nur Rohmad <arinadi.nur@gmail.com>
2024-03-04 00:37:06 +0700

    update PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "rhinoresolve-v1"
+const cacheName = "rhinoresolve-v1-1"
 const preCache = [
   "/",
   "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap",
